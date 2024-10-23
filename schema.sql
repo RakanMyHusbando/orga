370b9228d876b77a5b44391a67d292359e10df45
RakanMyHusbando <hellwing.jonas@web.de>
2024-10-23 10:18:49 +0200

    add schema.sql

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS User (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+CREATE TABLE IF NOT EXISTS AccountLeagueOfLegends (
+    user_id INTEGER NOT NULL,
+    name TEXT,
+    FOREIGN KEY (user_id) REFERENCES User(id),
+);
+CREATE TABLE IF NOT EXISTS UserLeagueOfLegends (
+    user_id INTEGER NOT NULL UNIQUE,
+    main_role TEXT,
+    second_role TEXT,
+    champ_0 TEXT,
+    champ_1 TEXT,
+    champ_2 TEXT,
+    FOREIGN KEY (user_id) REFERENCES User(id)
+);
--- storage.go
+++ storage.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	"errors"
+	"os"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -20,37 +22,25 @@ type SQLiteStorage struct {
 }
 
 func NewSQLiteStorage(dbFile string) (*SQLiteStorage, error) {
-	querys := []string{
-		`CREATE TABLE IF NOT EXISTS User (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			name TEXT
-		)`,
-		`CREATE TABLE IF NOT EXISTS AccountLeagueOfLegends (
-			user_id INTEGER NOT NULL,
-			name TEXT,
-			FOREIGN KEY (user_id) REFERENCES User(id)
-		)`,
-		`CREATE TABLE IF NOT EXISTS UserLeagueOfLegends (
-			user_id INTEGER NOT NULL UNIQUE,
-			main_role TEXT,
-			second_role TEXT,
-			champ_0 TEXT,
-			champ_1 TEXT,
-			champ_2 TEXT,
-			FOREIGN KEY (user_id) REFERENCES User(id)
-		)`,
-	}
 	db, err := sql.Open("sqlite3", dbFile)
 	if err != nil {
 		return nil, err
 	}
-	for i := range querys {
-		_, err := db.Exec(querys[i])
+
+	byteContent, err := os.ReadFile("schema.sql")
+	if err != nil {
+		return nil, err
+	}
+
+	queries := strings.Split(string(byteContent), ";")
+	for i := range queries {
+		query := strings.TrimSpace(queries[i])
+		_, err := db.Exec(query)
 		if err != nil {
-			newErr := "Error: " + err.Error() + "\nQuery:" + querys[i]
-			return nil, errors.New(newErr)
+			return nil, errors.New(err.Error() + " [Table:" + strings.Split(query, " ")[5] + "]")
 		}
 	}
+
 	defer db.Close()
 
 	return &SQLiteStorage{
